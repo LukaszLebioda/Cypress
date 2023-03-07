a4bfa8f74944594654fe9516db387cec0627946a
LukaszLebioda <lukasz.leb@gmail.com>
2023-03-07 10:49:37 +0100

    get, find, first-last, filter, parent

--- cypress/e2e/0_archive MAIN/aavaria2_basiccommands.cy.js
+++ cypress/e2e/0_archive MAIN/aavaria2_basiccommands.cy.js
@@ -1,26 +1,60 @@
 /* BASIC:
 cy.get();
 cy.pause();
+cy.title();
 cy.visit();
 cy.wait();
 */
 
 /* ADVANCED:
+cy.location("protocol", "host = hostname", "pathname");
 cy.screenshot (full page or single element);
-cy.scrollIntoView;
+cy.scrollIntoView();
 */
 
 
 describe("loading the page & asserting", () => {
 
+  let booksLength = 11;
+  let pageUrl = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html";
+  let username = "Lucas";
+  let password = "Lucas1982";
+
+  beforeEach( () => {
+    cy.visit(pageUrl);
+  })
+
+  // VISIT, URL, TITLE
   it("should load books-to-scrape 'Travel' website", () => {
     cy.visit("https://books.toscrape.com/catalogue/category/books/travel_2/index.html", { timeout: 10000 });
     cy.url().should("include", "travel"); // contain, eq, equal
-    cy.get("h1").contains("Travel").should("be.visible");
+    cy.title().should("eq", "Travel | Books to Scrape - Sandbox");
   })
 
-  it("should display correct number of books", () => {
-      cy.get(".product_pod").should("have.length", 11) 
+  // GET (CSS SELECTORS, FIRST/EQ/LAST, FILTER, FIND, PARENT)
+  it("should get elements by different css selectors", () => {
+
+    cy.get("h1").contains("Travel").should("be.visible"); // tag name
+    cy.get("#promotions").should("exist"); // id
+    cy.get("div#promotions").should("exist"); // tag name + id
+    cy.get(".instock").should("be.visible"); // class
+    cy.get("p.instock").should("be.visible"); // tag name + class
+    cy.get("[role='alert']").should("be.visible"); // attribute
+    cy.get("div[role='alert']").should("be.visible"); // tag name + attribute
+
+    cy.get("button").first().click(); // first position in an array of buttons
+    cy.get("button").eq(1).click(); // second position in an array of buttons (etc.)
+    cy.get("button").last().click(); // last position in an array of buttons
+    
+    cy.get(".price_color").filter(":contains('£36.94')").click(); // filters by attribute-value
+    cy.get("ul").find("li").last().should("be.visible"); // finds elements within other elements
+    cy.get("ul").parent().should("have.class", "side_categories"); // finds parents of elements
+
+  })
+  
+  // LENGTH OF ELEMENTS (with variable)
+  it.skip("should display correct number of books", () => {
+      cy.get(".product_pod").should("have.length", booksLength); 
   })
 
   // SCREENSHOTS
@@ -30,10 +64,17 @@ describe("loading the page & asserting", () => {
   })
  
   // SCROLLINTOVIEW + WAIT + PAUSE
-  it("should scroll into element", () => {
+  it.skip("should scroll into element", () => {
       cy.get("a").contains("Crime").scrollIntoView();
       cy.wait(2000); // cy.pause();
       cy.get("a").contains("Travel").scrollIntoView();
   })
+
+  // LOCATION
+  it.skip("should validate location elements", () => {
+    cy.location("protocol").should("contains", "https");
+    cy.location("host").should("equal", "books.toscrape.com");
+    cy.location("pathname").should("equal", "/catalogue/category/books/travel_2/index.html");
+})
   
 })
--- cypress/e2e/practice_now.cy.js
+++ cypress/e2e/practice_now.cy.js
@@ -1,2 +1,27 @@
+describe("Location demo", () => {
 
+    beforeEach ( () => {
+        cy.visit("https://www.saucedemo.com/");
+    })
+
+    it("should have title tag witha certain value", () => {
+        cy.title().should("eq", "Swag Labs");
+    })
+    it("should have a proper URL", () => {
+        cy.url().should("eq", "https://www.saucedemo.com/");
+    })
+    it("should have a proper https protocol", () => {
+        cy.location("protocol").should("contains", "https");
+        cy.location("host").should("contains", "www.saucedemo.com");
+    })
+    it("should redirect to proper /sublocation", () => {
+        cy.get("[data-test='user-name']").type("standard_user");
+        cy.get("[data-test='password']").type("secret_sauce");
+        cy.get("[data-test='password']").type("secret_sauce");
+        cy.get("[data-test='login-button']").click();
+        cy.location("pathname").should("eq", "/inventory.html");
+
+    })
+
+})
 
